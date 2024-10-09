7cc59ad2085cc71cfcaa1d7c83e1539ad10dc91b
amr89-dev <amr89.dev@gmail.com>
2024-10-09 16:18:50 -0500

    Update email sending logic to include dynamic dealer info and item details; enhance EmailTemplate for better layout and styling

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,13 +3,14 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req: Request) {
+  const { items, dealerInfo } = await req.json();
   try {
     const { data, error } = await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["virtual@musicalcedar.com"],
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }),
+      from: "Distribuidores Cedar <distribuidores@musicalcedar.com>",
+      to: [dealerInfo.email, "distribuidores@musicalcedar.com"],
+      subject: "Confirmación de pedido",
+      react: EmailTemplate({ items, dealerInfo }),
     });
 
     if (error) {
